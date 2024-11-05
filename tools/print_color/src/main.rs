2e294dcfdd901a2bbf93909eb278d3af1635cf2e
Liam Gallagher <ljpgallagher@xtra.co.nz>
2024-11-06 11:48:56 +1300

    Add `print_color` tool

---
+++ tools/print_color/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use lib::color::ColorSensor;
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    let i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.d20.into_pull_up_input(),
+        pins.d21.into_pull_up_input(),
+        50000,
+    );
+
+    let mut color_sensor = ColorSensor::new(i2c).unwrap();
+
+    loop {
+        let color = color_sensor.read().unwrap();
+        ufmt::uwriteln!(&mut serial, "Color: {:?}", color).unwrap();
+    }
+}
